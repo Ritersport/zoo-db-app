48169f83d14845a988eed05aed919ac4821273de
Ritersport <Ritersport>
2023-05-25 04:57:28 -0400

    added employee insertion

--- app/src/main/java/ru/nsu/databases/data/repository/database/daos/employee/EmployeeDaoimpl.kt
+++ app/src/main/java/ru/nsu/databases/data/repository/database/daos/employee/EmployeeDaoimpl.kt
@@ -24,41 +24,53 @@ class EmployeeDaoImpl @Inject constructor(
                 "SELECT * FROM \"Employees\" JOIN (\n" +
                         "      \tSELECT \"Id\" AS profession_id, \"Name\" AS profession_name\n" +
                         "      \tFROM \"Professions\")\n" +
-                        "    \tON (\"Profession\" = profession_id)\n"
+                        "      \tON (\"Profession\" = profession_id)\n"
             )
 
             val result: MutableList<Employee> = mutableListOf()
             while (rawResult.next()) {
-                result.add(
-                    Employee(
-                        id = rawResult.getInt("Id"),
-                        gender = rawResult.getString("Gender"),
-                        name = rawResult.getString("First_name"),
-                        surname = rawResult.getString("Last_name"),
-                        patronymic = rawResult.getString("Patronymic"),
-                        birthDate = rawResult.getDate("Birthdate"),
-                        profession = Profession(
-                            id = rawResult.getInt("PROFESSION_ID"),
-                            name = rawResult.getString("PROFESSION_NAME")
-                        ),
-                        salary = rawResult.getInt("Salary"),
-                        dismissalDate = rawResult.getDate("Dismissal_date"),
-                        employmentDate = rawResult.getDate("Employment_date"),
-                    )
-                )
+                result.add(Employee(
+                    id = rawResult.getInt("Id"),
+                    gender = rawResult.getString("Gender"),
+                    name = rawResult.getString("First_name"),
+                    surname = rawResult.getString("Last_name"),
+                    patronymic = rawResult.getString("Patronymic"),
+                    birthDate = rawResult.getDate("Birthdate"),
+                    profession = Profession(id = rawResult.getInt("PROFESSION_ID"),
+                        name = rawResult.getString("PROFESSION_NAME")),
+                    salary = rawResult.getInt("Salary"),
+                    dismissalDate = rawResult.getDate("Dismissal_date"),
+                    employmentDate = rawResult.getDate("Employment_date"),
+                ))
             }
             return result
         }
 
-    override fun addOrUpdate(employee: Employee): Completable {
-        TODO("Not yet implemented")
+    override fun addOrUpdate(employee: Employee): Completable = Completable.fromAction {
+        addOrUpdateBlocking(employee)
     }
 
-    override fun getById(id: Int): Maybe<Employee> {
-        TODO("Not yet implemented")
-    }
+    private fun addOrUpdateBlocking(employee: Employee) =
+        connectionProvider.openConnection().use { connection ->
+            val statement = connection.createStatement()
+            statement.executeQuery(
+                "BEGIN " +
+                        "\"Add_employee\" (" +
+                        "'${employee.gender}', " +
+                        "'${employee.name}', " +
+                        "'${employee.surname}'," +
+                        "'${employee.patronymic}'," +
+                        "'06-MAR-2001'," +
+                        "${employee.profession.id}," +
+                        "${employee.salary}," +
+                        "'25-MAY-2023'," +
+                        "'null'); " +
+                        "END;"
+            )
+        }
 
-    override fun removeById(id: Int): Completable {
-        TODO("Not yet implemented")
-    }
+
+    override fun getById(id: Int): Maybe<Employee> = Maybe.empty()
+
+    override fun removeById(id: Int): Completable = Completable.complete()
 }
