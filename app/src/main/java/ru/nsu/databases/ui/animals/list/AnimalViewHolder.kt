a4c7887ee4b236296cd89c91bdbe37045e65761e
Ritersport <leomkrita2001@gmail.com>
2023-05-25 23:39:16 +0700

    finished animal list view

--- app/src/main/java/ru/nsu/databases/ui/animals/list/AnimalViewHolder.kt
+++ app/src/main/java/ru/nsu/databases/ui/animals/list/AnimalViewHolder.kt
@@ -1,5 +1,6 @@
 package ru.nsu.databases.ui.animals.list
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import ru.nsu.databases.databinding.AnimalShortItemBinding
 import ru.nsu.databases.domain.model.zoo.Animal
@@ -12,14 +13,16 @@ class AnimalViewHolder(
         binding.run {
             name.text = animal.animal.name
             kind.text = animal.animal.kind.name
+            gender.text = animal.animal.gender.name
             binding.setWarning(animal.migrationNeeded)
 
             binding.root.setOnClickListener { onItemClicked(animal.animal) }
         }
 
-    fun AnimalShortItemBinding.setWarning(warn: Boolean) = if (warn) {
-
+    private fun AnimalShortItemBinding.setWarning(warn: Boolean) = if (warn) {
+        this.warning.visibility = View.VISIBLE
     } else {
-
+        this.warning.visibility = View.GONE
     }
+
 }
